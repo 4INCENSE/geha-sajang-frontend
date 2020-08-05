a0dfa4487a54f4864107acb71aa720937f50629f
Joy <hyewon3938@naver.com>
2020-08-05 21:18:29 +0900

    [#1] feat : style 리셋
    - styled-reset install
    - GlobalStyleProvider 컴포넌트를 추가해 전체 스타일 리셋

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import GlobalStyleProvider from '@/components/style/GlobalStyle';
 import LogIn from '@/components/LogIn/LogIn';
 
 const App = () => {
-  return <LogIn />;
+  return (
+    <GlobalStyleProvider>
+      <LogIn />
+    </GlobalStyleProvider>
+  );
 };
 
 export default App;
