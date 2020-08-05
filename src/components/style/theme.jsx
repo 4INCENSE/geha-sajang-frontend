63cf0672515349fa8c441bd33fd26a4ca6d9ae48
Joy <hyewon3938@naver.com>
2020-08-05 21:19:18 +0900

    [#1] feat : 스타일 테마 파일 추가
    - font-size 와 color를 담은 테마 파일 추가

---
+++ src/components/style/theme.jsx
@@ -0,0 +1,14 @@
+const fontSize = {
+  micro: '8px',
+  small: '12px',
+  base: '14px',
+  ragular: '16px',
+  large: '20px',
+  xLarge: '24px',
+  title: '32px'
+};
+const color = {
+  point: '#3987FF'
+};
+
+export default { fontSize, color };
