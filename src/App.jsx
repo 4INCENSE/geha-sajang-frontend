2c9b61d461a6515aa4ad3377d0a139c6e99e7258
Joy <hyewon3938@naver.com>
2020-08-11 17:19:15 +0900

    [#3] feat : 리액트 라우팅 설정
    - App.js에 라우팅 설정
    - 지정해둔 요청 이외의 요청은 루트로 리다이렉션

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import { Route, Switch, Redirect } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+
 import GlobalStyleProvider from '@/components/style/GlobalStyle';
 import LogIn from '@/components/LogIn/LogIn';
+import Main from '@/components/Main/Main';
+import SignUp from '@/components/SignUp/SignUp';
 import Footer from '@/components/Footer/Footer';
 
 const App = () => {
   return (
     <GlobalStyleProvider>
-      <LogIn />
-      <Footer/>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Main} />
+          <Route path="/LogIn" component={LogIn} />
+          <Route path="/SignUp" component={SignUp} />
+          <Redirect path="*" to="/" />
+        </Switch>
+      </BrowserRouter>
+      <Footer />
     </GlobalStyleProvider>
   );
 };
