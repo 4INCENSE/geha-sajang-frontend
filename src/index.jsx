dcf6630de1cc8afa7c05b851497dcba04bb5c270
Joy <hyewon3938@naver.com>
2020-08-14 05:25:43 +0900

    [#4] feat : 상태 관리를 위한 redux와 redux-thunk 미들웨어 설정
    - react-redux 설치
    - index.jsx에 리듀서와 스토어, 미들웨어 등록

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,30 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+
+import { loggerMiddleware } from '@/redux/middleware/loggerMiddleware';
+import { resisterGuestHouseInfoMiddleware } from '@/redux/middleware/resisterGuestHouseInfoMiddleware';
+
+import { resisterGuestHouseInfoReducer } from '@/redux/reducers/resisterGuestHouseInfoReducer';
 
 const rootElement = document.getElementById('root');
 
-import('@/App').then(({ default: App }) => ReactDOM.render(<App />, rootElement));
+const rootReducer = combineReducers({
+  resisterGuestHouseInfoReducer
+});
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(loggerMiddleware, resisterGuestHouseInfoMiddleware))
+);
+
+import('@/App').then(({ default: App }) =>
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    rootElement
+  )
+);
