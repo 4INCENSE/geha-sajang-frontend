7ab0b70dfdfc5cfd28b3004aa6b0a51a49b44839
Joy <hyewon3938@naver.com>
2020-08-18 21:45:52 +0900

    [#2] feat : 메인 로그인화면 UI 수정
    - 로그인화면 이미지 변경
    - 로그인화면 이미지 색감 변경
    - 로그인화면 설명 변경
    - 사이트 시작하기 버튼 변경
    - 시작하기 버튼 클릭 시 로그인 폼 보여주도록 구현

--- src/components/LogIn/LogIn.jsx
+++ src/components/LogIn/LogIn.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+
 import logo from '@/img/Logo/logo.png';
 
 import LogInForm from '@/components/LogIn/LogInForm/LogInForm';
@@ -9,10 +10,11 @@ const Wrap = styled.div`
   position: relative;
   display: flex;
   flex-direction: column;
-  width: 100%;
   justify-content: center;
   align-items: center;
+  width: 100%;
   margin-bottom: 100px;
+  overflow-x: hidden;
 `;
 
 const LogInConentWrap = styled.div`
@@ -23,68 +25,103 @@ const LogInConentWrap = styled.div`
   height: 680px;
   background-image: url('bunkBed.jpg');
   background-size: cover;
+  overflow-x: hidden;
+`;
+
+const ImgBlur = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.6);
 `;
 
 const Logo = styled.img`
   position: absolute;
   top: 20px;
   left: 60px;
-  width: 280px;
+  width: 300px;
+  padding: 10px;
 `;
 
-const DescriptionContentWrap = styled.div`
+const ServiceStartButton = styled.button`
   position: absolute;
+  bottom: 50px;
+  right: 50px;
+  display: 'flex';
+  justify-content: center;
+  align-items: center;
+  width: 300px;
+  padding: 30px 40px;
+  background: ${({ theme }) => theme.color.point};
+  border-radius: 5px;
+  font-family: 'S-CoreDream-4Regular';
+  color: white;
+  font-size: 25px;
+  font-weight: bold;
+  &:hover {
+    background: black;
+  }
+`;
+
+const DescriptionContent = styled.div`
+  position: absolute;
+  bottom: 50px;
+  left: 50px;
   display: flex;
   flex-direction: column;
-  background: rgba(0, 0, 0, 0.7);
-  bottom: 30px;
-  left: 40px;
-  padding: 35px;
-`;
-
-const EnglishContent = styled.div`
-  color: white;
-  font-weight: bold;
-  font-size: 55px;
-  text-align: left;
-`;
-
-const KoreanContent = styled.div`
+  font-family: 'S-CoreDream-4Regular';
   color: white;
   font-weight: bold;
   font-size: 23px;
+  line-height: 40px;
   text-align: left;
-  font-family: 'S-CoreDream-4Regular';
-  margin-bottom: 10px;
 `;
 
 const LoginFormWrap = styled.div`
   position: absolute;
+  right: -650px;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  width: 40%;
+  width: 650px;
   height: 100%;
-  top: 0;
-  right: 0;
-  background: rgba(0, 0, 0, 0.75);
+  transition: 0.5s;
 `;
 
 const LogIn = () => {
+  const [logInFormWrapRightValue, setLogInFormWrapRightValueValue] = useState('-650px');
+  const [serviceStartDisplay, setServiceStartButtonDisplay] = useState('flex');
+
+  const startButtonClickHandler = () => {
+    setLogInFormWrapRightValueValue(0);
+    setServiceStartButtonDisplay('none');
+  };
+
+  const cancelButtonClickHandler = () => {
+    setLogInFormWrapRightValueValue('-650px');
+    setTimeout(() => {
+      setServiceStartButtonDisplay('flex');
+    }, 230);
+  };
+
   return (
     <Wrap>
       <LogInConentWrap>
+        <ImgBlur />
         <Logo src={logo} />
-        <DescriptionContentWrap>
-          <KoreanContent>게스트하우스 예약관리 서비스</KoreanContent>
-          <EnglishContent>
-            Guest house
-            <br />
-            Reservation <br /> management service
-          </EnglishContent>
-        </DescriptionContentWrap>
-        <LoginFormWrap>
-          <LogInForm />
+        <DescriptionContent>
+          게스트 하우스 맞춤형 예약 관리 시스템으로
+          <br />
+          게스트의 예약을 보다 손쉽게 관리하세요
+        </DescriptionContent>
+        <ServiceStartButton style={{ display: serviceStartDisplay }} onClick={startButtonClickHandler}>
+          지금 바로 시작하기
+        </ServiceStartButton>
+        <LoginFormWrap style={{ right: logInFormWrapRightValue }}>
+          <LogInForm cancelButtonClickHandler={cancelButtonClickHandler} />
         </LoginFormWrap>
       </LogInConentWrap>
       <ServiceDescription />
