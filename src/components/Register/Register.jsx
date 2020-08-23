6b0daf11a37ce84ac6990218e6b24e8cfd6c17ff
Joy <hyewon3938@naver.com>
2020-08-23 22:33:23 +0900

    [#6] feat : 회원가입 버튼 핸들러에 호스트 추가 등록 UI 이동 코드 추가
    -  게스트하우스 등록 여부 확인 컴포넌트에서 등록된 게스트하우스 버튼 클릭 시 호스트 추가 컴포넌트의 display 활성화

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -5,6 +5,7 @@ import Header from '@/components/Header/Header';
 import TermsAndConditions from '@/components/Register/TermsAndConditions/TermsAndConditions';
 import SelectRegistrationType from '@/components/Register/SelectRegistrationType/SelectRegistrationType';
 import RegisterGuestHouseInfo from '@/components/Register/RegisterGuestHouseInfo/RegisterGuestHouseInfo';
+import RegisterRegisteredGuesthouse from '@/components/Register/RegisterRegisteredGuesthouse/RegisterRegisteredGuesthouse';
 
 const Wrap = styled.div`
   display: flex;
@@ -18,34 +19,47 @@ const Register = () => {
   const [termsAndConditionDisplay, setTermsAndConditionDisplay] = useState('flex');
   const [selectRegistrationTypeDisplay, setSelectRegistrationTypeDisplay] = useState('none');
   const [registerHouseInfoDisplay, setRegisterHouseInfoDisplay] = useState('none');
+  const [registerRegisteredGuesthouseDisplay, setRegisterRegisteredGuesthouseDisplay] = useState('none');
 
-  const termsAndConditionButtonClickHandler = () => {
+  const goToSelectRegistrationType = () => {
     setTermsAndConditionDisplay('none');
     setSelectRegistrationTypeDisplay('flex');
     setRegisterHouseInfoDisplay('none');
+    setRegisterRegisteredGuesthouseDisplay('none');
   };
 
-  const selectRegistrationTypeButtonClickHandler = () => {
+  const goToRegisterGuestHouseInfo = () => {
     setTermsAndConditionDisplay('none');
     setSelectRegistrationTypeDisplay('none');
     setRegisterHouseInfoDisplay('flex');
+    setRegisterRegisteredGuesthouseDisplay('none');
   };
 
-  const registerHouseInfoButtonClickHandler = () => {
+  const goToRegisterRegisteredGuestHouse = () => {
     setTermsAndConditionDisplay('none');
     setSelectRegistrationTypeDisplay('none');
     setRegisterHouseInfoDisplay('none');
+    setRegisterRegisteredGuesthouseDisplay('flex');
+  };
+
+  const goToRegisterRoomInfo = () => {
+    setTermsAndConditionDisplay('none');
+    setSelectRegistrationTypeDisplay('none');
+    setRegisterHouseInfoDisplay('none');
+    setRegisterRegisteredGuesthouseDisplay('none');
   };
 
   return (
     <Wrap>
       <Header />
-      <TermsAndConditions display={termsAndConditionDisplay} nextButton={termsAndConditionButtonClickHandler} />
+      <TermsAndConditions display={termsAndConditionDisplay} nextButton={goToSelectRegistrationType} />
       <SelectRegistrationType
         display={selectRegistrationTypeDisplay}
-        nextButton={selectRegistrationTypeButtonClickHandler}
+        registerGuestHouseInfoButton={goToRegisterGuestHouseInfo}
+        registeredGuestHouseButton={goToRegisterRegisteredGuestHouse}
       />
-      <RegisterGuestHouseInfo display={registerHouseInfoDisplay} nextButton={registerHouseInfoButtonClickHandler} />
+      <RegisterGuestHouseInfo display={registerHouseInfoDisplay} nextButton={goToRegisterRoomInfo} />
+      <RegisterRegisteredGuesthouse display={registerRegisteredGuesthouseDisplay} nextButton={goToRegisterRoomInfo} />
     </Wrap>
   );
 };
