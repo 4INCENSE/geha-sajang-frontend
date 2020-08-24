8365d24cc905dc528eff0d931a9666e0cec29cb5
Joy <hyewon3938@naver.com>
2020-08-24 21:28:30 +0900

    feat : 이미지 업로드 UI 독립된 컴포넌트로 분리
    - 이미지 업로드 UI를 독립된 컴포넌트로 분리
    - 이미지 업로드 조건 검사 기능도 컴포넌트로 함께 분리

--- src/components/Register/RegisterGuestHouseInfo/RegisterGuestHouseInfo.jsx
+++ src/components/Register/RegisterGuestHouseInfo/RegisterGuestHouseInfo.jsx
@@ -7,23 +7,20 @@ import { addExtraService, deleteExtraService } from '@/redux/actions/extraServic
 
 import { checkDuplicate } from '@/lib/util/checkDuplicate';
 
-import defaultPreviewImage from '@/img/default_preview_img.png';
 import questionIcon from '@/img/icon/question.png';
 import closeIcon from '@/img/icon/close_w.png';
 
 import Button from '@/components/Button/Button';
+import UploadFile from '@/components/UIComponents/UploadFile/UploadFile';
 
 const RegisterGuestHouseInfo = ({ display, nextButton }) => {
   const dispatch = useDispatch();
   const { extraServiceList } = useSelector((state) => state.registerGuestHouseInfoReducer);
 
-  const [currentImage, setCurrentImage] = useState(defaultPreviewImage);
-  const [currentImageName, setCurrentImageName] = useState('파일을 업로드 해주세요');
+  const [currentImage, setCurrentImage] = useState();
   const [serviceList, setServiceList] = useState([]);
   const [nameMessage, setNameMessage] = useState();
   const [nameMessageDisplay, setNameMessageDisplay] = useState('none');
-  const [imageMessage, setImageMessage] = useState();
-  const [imageMessageDisplay, setImageMessageDisplay] = useState('none');
   const [numberMessage, setNumberMessage] = useState();
   const [numberMessageDisplay, setNumberMessageDisplay] = useState('none');
   const [serviceMessage, setServiceMessage] = useState();
@@ -60,40 +57,6 @@ const RegisterGuestHouseInfo = ({ display, nextButton }) => {
     }
     setNameMessageDisplay('none');
     return true;
-  };
-
-  const uploadImage = (e) => {
-    const currentFile = e.target.files[0];
-    if (!currentFile) return;
-    if (!validateImageFileType(currentFile)) {
-      setImageMessage('이미지 파일 유형은 png, jpg, jpeg만 가능합니다');
-      setImageMessageDisplay('block');
-      setCurrentImage(defaultPreviewImage);
-      setCurrentImageName('파일을 업로드 해주세요');
-      return;
-    }
-    setImageMessageDisplay('none');
-    setCurrentImage(URL.createObjectURL(currentFile));
-    setCurrentImageName(currentFile.name);
-    // formData 생성은 currentImage를 통해서 나중에 하기
-    //  const formData = new FormData();
-    // formData.append('uploadImage', e.target.files[0], 'userName');
-    //axios로 서버에 전송 시 header에 아래와 같이 추가하여 전송해야함
-    // const config = {
-    //   headers: {
-    //     'content-type': 'multipart/form-data'
-    //   }
-    // };
-  };
-  const deleteImage = () => {
-    setCurrentImage(defaultPreviewImage);
-    setCurrentImageName('파일을 업로드 해주세요');
-  };
-
-  const validateImageFileType = (file) => {
-    const fileTypes = ['image/jpeg', 'image/jpg', 'image/png'];
-
-    return fileTypes.includes(file.type);
   };
 
   const validatePhoneNumber = () => {
@@ -174,30 +137,6 @@ const RegisterGuestHouseInfo = ({ display, nextButton }) => {
       </RegisterSubTitle>
       <RegisterWrap>
         <InputWrap>
-          <InputTitle>
-            게스트하우스 이름<span> ●</span>
-          </InputTitle>
-          <Input onBlur={validateName} ref={nameInput} />
-          <InputMessage style={{ display: nameMessageDisplay }}>{nameMessage}</InputMessage>
-        </InputWrap>
-        <InputWrap>
-          <InputTitle>게스트하우스 대표이미지</InputTitle>
-          <InputButtonWrap>
-            <UploadButton htmlFor="file">업로드</UploadButton>
-            <UploadImageValue>{currentImageName}</UploadImageValue>
-            <InputButton onClick={deleteImage}>삭제</InputButton>
-            <input
-              style={{ position: 'absolute', width: '0', height: '0' }}
-              id="file"
-              type="file"
-              accept=".jpg, .jpeg, .png"
-              onChange={uploadImage}
-            />
-          </InputButtonWrap>
-          <InputMessage style={{ display: imageMessageDisplay }}>{imageMessage}</InputMessage>
-          <PreviewWrap>
-            <Preview src={currentImage} />
-          </PreviewWrap>
         </InputWrap>
         <InputWrap>
           <InputTitle>
@@ -372,55 +311,6 @@ const InputButton = styled.button`
   &:hover {
     border: 2px solid ${({ theme }) => theme.color.point};
     color: ${({ theme }) => theme.color.point};
-  }
-`;
-
-const PreviewWrap = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 280px;
-  margin-top: 15px;
-`;
-const Preview = styled.img`
-  max-width: 100%;
-  height: 100%;
-`;
-
-const UploadButton = styled.label`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-family: 'Eoe_Zno_L';
-  width: 80px;
-  height: 40px;
-  color: ${({ theme }) => theme.color.darkGray};
-  font-weight: bold;
-  font-size: 14px;
-  border-radius: 5px;
-  border: 1px solid ${({ theme }) => theme.color.lightGray};
-  border-radius: 5px;
-  margin-right: 10px;
-  &:hover {
-    border: 2px solid ${({ theme }) => theme.color.point};
-    color: ${({ theme }) => theme.color.point};
-  }
-`;
-
-const UploadImageValue = styled.div`
-  display: flex;
-  align-items: center;
-  width: 260px;
-  height: 40px;
-  padding: 10px;
-  border-radius: 5px;
-  border: 1px solid ${({ theme }) => theme.color.lightGray};
-  overflow: auto;
-  scrollbar-width: none;
-  -ms-overflow-style: none;
-  ::-webkit-scrollbar {
-    display: none;
   }
 `;
 
