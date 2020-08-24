14dd2a14f7b16ea201ef579f4bb45e94f12341e6
Joy <hyewon3938@naver.com>
2020-08-24 22:15:12 +0900

    [#11] feat : 회원가입 이메일 주소 조건 검사
    - 이메일 형식 검사
    - 이메일 필수 입력 검사

--- src/components/Register/CreateAccount/CreateAccount.jsx
+++ src/components/Register/CreateAccount/CreateAccount.jsx
@@ -10,6 +10,30 @@ import UploadFile from '@/components/UIComponents/UploadFile/UploadFile';
 
 const CreateAccount = () => {
   const [currentImage, setCurrentImage] = useState();
+  const [emailMessage, setEmailMessage] = useState();
+  const [emailMessageDisplay, setEmailMessageDisplay] = useState();
+  const emailInput = React.createRef();
+  const passwordInput = React.createRef();
+  const passwordConfirmInput = React.createRef();
+  const nicknameInput = React.createRef();
+
+  const validateEmail = () => {
+    const email = emailInput.current.value;
+    var regExp = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+
+    if (email.length <= 0) {
+      setEmailMessage('이메일 입력은 필수입니다');
+      setEmailMessageDisplay('block');
+      return false;
+    }
+    if (!regExp.test(email)) {
+      setEmailMessage('이메일을 다시 확인해주세요');
+      setEmailMessageDisplay('block');
+      return false;
+    }
+    setEmailMessageDisplay('none');
+    return true;
+  };
 
   return (
     <Wrap>
@@ -18,7 +42,14 @@ const CreateAccount = () => {
         <RegisterTitle>회원가입</RegisterTitle>
         <RegisterWrap>
           <InputWrap>
-            <TitleInput title="이메일 주소" spanValue=" ●" />
+            <TitleInput
+              title="이메일 주소"
+              spanValue=" ●"
+              refValue={emailInput}
+              onBlur={validateEmail}
+              messageDisplay={emailMessageDisplay}
+              messageValue={emailMessage}
+            />
           </InputWrap>
           <InputWrap>
             <TitleInput title="비밀번호" spanValue=" ●" />
