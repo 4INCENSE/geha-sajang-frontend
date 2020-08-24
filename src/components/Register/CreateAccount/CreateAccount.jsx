a19b92e6c9b60a78ec31051ed10d5806fd55b192
Joy <hyewon3938@naver.com>
2020-08-25 01:34:23 +0900

    feat : 약관동의 후 회원가입 폼으로 넘어가는 기능 구현
    - 약관동의 컴포넌트와 회원가입 컴포넌트의 display 값을 이용해 /register 페이지에서 이동 구현

--- src/components/Register/CreateAccount/CreateAccount.jsx
+++ src/components/Register/CreateAccount/CreateAccount.jsx
@@ -2,13 +2,11 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import { useHistory } from 'react-router-dom';
 
-import Header from '@/components/Header/Header';
-
 import BlackButton from '@/components/UIComponents/Button/BlackButton';
 import TitleInput from '@/components/UIComponents/Input/TitleInput';
 import UploadFile from '@/components/UIComponents/UploadFile/UploadFile';
 
-const CreateAccount = () => {
+const CreateAccount = ({ display }) => {
   const history = useHistory();
 
   const [currentImage, setCurrentImage] = useState();
@@ -119,70 +117,60 @@ const CreateAccount = () => {
   };
 
   return (
-    <Wrap>
-      <Header />
-      <ContentWrap>
-        <RegisterTitle>회원가입</RegisterTitle>
-        <RegisterWrap>
-          <InputWrap>
-            <TitleInput
-              title="이메일 주소"
-              spanValue=" ●"
-              refValue={emailInput}
-              onBlur={validateEmail}
-              messageDisplay={emailMessageDisplay}
-              messageValue={emailMessage}
-            />
-          </InputWrap>
-          <InputWrap>
-            <TitleInput
-              type="password"
-              title="비밀번호"
-              spanValue=" ●"
-              refValue={passwordInput}
-              onBlur={validatePassword}
-              messageDisplay={passwordMessageDisplay}
-              messageValue={passwordMessage}
-            />
-          </InputWrap>
-          <InputWrap>
-            <TitleInput
-              type="password"
-              title="비밀번호 확인"
-              spanValue=" ●"
-              onBlur={confirmPassword}
-              refValue={passwordConfirmInput}
-              messageDisplay={passwordConfirmMessageDisplay}
-              messageValue={passwordConfirmMessage}
-            />
-          </InputWrap>
-          <InputWrap>
-            <TitleInput
-              title="닉네임"
-              spanValue=" ●"
-              onBlur={validateNickname}
-              refValue={nicknameInput}
-              messageDisplay={nicknameMessageDisplay}
-              messageValue={nicknameMessage}
-            />
-          </InputWrap>
-          <UploadFile title="프로필 사진" getCurrentFile={setCurrentImage} />
-          <BlackButton title="회원가입" onClick={registerButtonClickHandler} />
-        </RegisterWrap>
-      </ContentWrap>
-    </Wrap>
+    <ContentWrap style={{ display: display }}>
+      <RegisterTitle>회원가입</RegisterTitle>
+      <RegisterWrap>
+        <InputWrap>
+          <TitleInput
+            title="이메일 주소"
+            spanValue=" ●"
+            refValue={emailInput}
+            onBlur={validateEmail}
+            messageDisplay={emailMessageDisplay}
+            messageValue={emailMessage}
+          />
+        </InputWrap>
+        <InputWrap>
+          <TitleInput
+            type="password"
+            title="비밀번호"
+            spanValue=" ●"
+            refValue={passwordInput}
+            onBlur={validatePassword}
+            messageDisplay={passwordMessageDisplay}
+            messageValue={passwordMessage}
+          />
+        </InputWrap>
+        <InputWrap>
+          <TitleInput
+            type="password"
+            title="비밀번호 확인"
+            spanValue=" ●"
+            onBlur={confirmPassword}
+            refValue={passwordConfirmInput}
+            messageDisplay={passwordConfirmMessageDisplay}
+            messageValue={passwordConfirmMessage}
+          />
+        </InputWrap>
+        <InputWrap>
+          <TitleInput
+            title="닉네임"
+            spanValue=" ●"
+            onBlur={validateNickname}
+            refValue={nicknameInput}
+            messageDisplay={nicknameMessageDisplay}
+            messageValue={nicknameMessage}
+          />
+        </InputWrap>
+        <UploadFile title="프로필 사진" getCurrentFile={setCurrentImage} />
+        <BlackButton title="회원가입" onClick={registerButtonClickHandler} />
+      </RegisterWrap>
+    </ContentWrap>
   );
 };
 
 export default CreateAccount;
 
-const Wrap = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 100%;
-  overflow-x: hidden;
-`;
 const ContentWrap = styled.div`
   display: flex;
   flex-direction: column;
@@ -191,6 +179,7 @@ const ContentWrap = styled.div`
   width: 100%;
   padding: 80px;
 `;
+
 const RegisterTitle = styled.div`
   font-family: 'S-CoreDream-5Medium';
   font-weight: bold;
@@ -199,6 +188,7 @@ const RegisterTitle = styled.div`
   font-weight: bold;
   margin: 0 0 40px 0;
 `;
+
 const RegisterWrap = styled.div`
   position: relative;
   display: flex;
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -2,10 +2,28 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 
 import Header from '@/components/Header/Header';
-import TermsAndConditions from '@/components/Register/TermsAndConditions/TermsAndConditions';
-import SelectRegistrationType from '@/components/Register/SelectRegistrationType/SelectRegistrationType';
-import RegisterGuestHouseInfo from '@/components/Register/RegisterGuestHouseInfo/RegisterGuestHouseInfo';
-import RegisterRegisteredGuesthouse from '@/components/Register/RegisterRegisteredGuesthouse/RegisterRegisteredGuesthouse';
+import CreateAccount from '@/components/Register/CreateAccount/CreateAccount';
+import TermsAndConditions from '@/components/Register/CreateAccount/TermsAndConditions/TermsAndConditions';
+
+const Register = () => {
+  const [TermsAndConditionsDisplay, setTermsAndConditionsDisplay] = useState('flex');
+  const [CreateAccountDisplay, setCreateAccountDisplay] = useState('none');
+
+  const nextButtonClickHandler = () => {
+    setTermsAndConditionsDisplay('none');
+    setCreateAccountDisplay('flex');
+  };
+
+  return (
+    <Wrap>
+      <Header />
+      <TermsAndConditions display={TermsAndConditionsDisplay} nextButtonClickHandler={nextButtonClickHandler} />
+      <CreateAccount display={CreateAccountDisplay} />
+    </Wrap>
+  );
+};
+
+export default Register;
 
 const Wrap = styled.div`
   display: flex;
@@ -14,54 +32,3 @@ const Wrap = styled.div`
   width: 100%;
   overflow-x: hidden;
 `;
-
-const Register = () => {
-  const [termsAndConditionDisplay, setTermsAndConditionDisplay] = useState('flex');
-  const [selectRegistrationTypeDisplay, setSelectRegistrationTypeDisplay] = useState('none');
-  const [registerHouseInfoDisplay, setRegisterHouseInfoDisplay] = useState('none');
-  const [registerRegisteredGuesthouseDisplay, setRegisterRegisteredGuesthouseDisplay] = useState('none');
-
-  const goToSelectRegistrationType = () => {
-    setTermsAndConditionDisplay('none');
-    setSelectRegistrationTypeDisplay('flex');
-    setRegisterHouseInfoDisplay('none');
-    setRegisterRegisteredGuesthouseDisplay('none');
-  };
-
-  const goToRegisterGuestHouseInfo = () => {
-    setTermsAndConditionDisplay('none');
-    setSelectRegistrationTypeDisplay('none');
-    setRegisterHouseInfoDisplay('flex');
-    setRegisterRegisteredGuesthouseDisplay('none');
-  };
-
-  const goToRegisterRegisteredGuestHouse = () => {
-    setTermsAndConditionDisplay('none');
-    setSelectRegistrationTypeDisplay('none');
-    setRegisterHouseInfoDisplay('none');
-    setRegisterRegisteredGuesthouseDisplay('flex');
-  };
-
-  const goToRegisterRoomInfo = () => {
-    setTermsAndConditionDisplay('none');
-    setSelectRegistrationTypeDisplay('none');
-    setRegisterHouseInfoDisplay('none');
-    setRegisterRegisteredGuesthouseDisplay('none');
-  };
-
-  return (
-    <Wrap>
-      <Header />
-      <TermsAndConditions display={termsAndConditionDisplay} nextButton={goToSelectRegistrationType} />
-      <SelectRegistrationType
-        display={selectRegistrationTypeDisplay}
-        registerGuestHouseInfoButton={goToRegisterGuestHouseInfo}
-        registeredGuestHouseButton={goToRegisterRegisteredGuestHouse}
-      />
-      <RegisterGuestHouseInfo display={registerHouseInfoDisplay} nextButton={goToRegisterRoomInfo} />
-      <RegisterRegisteredGuesthouse display={registerRegisteredGuesthouseDisplay} nextButton={goToRegisterRoomInfo} />
-    </Wrap>
-  );
-};
-
-export default Register;
