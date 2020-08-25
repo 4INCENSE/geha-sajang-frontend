7a337ea91eefabd96c3c23719616fc2bb26ce2c2
Joy <hyewon3938@naver.com>
2020-08-26 03:21:56 +0900

    [#12] feat : guestHouseInfo를 서버에 POST 요청하는 미들웨어 함수 구현
    - axios를 이용해 guestHouseInfo를 서버에 POST 하는 미들웨어 함수 구현

--- src/redux/middleware/registerGuestHouseInfoMiddleware.jsx
+++ src/redux/middleware/registerGuestHouseInfoMiddleware.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 
 import { setExtraServiceList } from '@/redux/actions/extraServiceAction';
 
@@ -6,6 +7,7 @@ const registerGuestHouseInfoMiddleware = (store) => (dispatch) => (action) => {
 
   if (action.type == 'ADD_EXTRA_SERVICE') return addExtraService(params);
   if (action.type == 'DELETE_EXTRA_SERVICE') return deleteExtraService(params);
+  if (action.type == 'POST_GUEST_HOUSE_INFO') return postGuestHouseInfo(params);
 };
 
 export { registerGuestHouseInfoMiddleware };
@@ -29,6 +31,24 @@ const deleteServiceFromArray = (store, payload) => {
   extraServiceList.splice(deleteIndex, 1);
   return extraServiceList;
 };
+
+const postGuestHouseInfo = ({ store, action }) => {
+  const payload = action.payload;
+  const { extraServiceList } = store.getState().registerGuestHouseInfoReducer;
+
+  const formData = new FormData();
+  formData.append('name', payload.name);
+  formData.append('mainNumber', payload.mainNumber);
+  formData.append('extra', putSeparator(extraServiceList));
+  formData.append('file', payload.image);
+
+  axios
+    .post(process.env.POST_GUESTHOUSE_INFO, formData, { headers: { 'Content-Type': 'multipart/form-data' } })
+    .then((response) => {
+      console.log(response);
+    });
+};
+
 const putSeparator = (array) => {
   const separator = '☆§♥♨☎';
   return array.join(separator);
