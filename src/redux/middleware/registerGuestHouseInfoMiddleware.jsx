423af5d5b9423bf5a0ea8ddade1dfb755e4d4fb9
Joy <hyewon3938@naver.com>
2020-08-26 03:16:29 +0900

    refactor : registerGuestHouseInfoMiddleware 코드 정리
    - 미들웨어의 if문 부분을 깔끔하게 정리하기 위해 if문마다  return하는 함수를 생성
    - 공통적인 파라미터를 하나의 변수로 관리
    - 파라미터를 destructuring 된 object로 전달

--- src/redux/middleware/registerGuestHouseInfoMiddleware.jsx
+++ src/redux/middleware/registerGuestHouseInfoMiddleware.jsx
@@ -1,18 +1,29 @@
+
 import { setExtraServiceList } from '@/redux/actions/extraServiceAction';
 
 const registerGuestHouseInfoMiddleware = (store) => (dispatch) => (action) => {
-  if (action.type == 'ADD_EXTRA_SERVICE') return dispatch(setExtraServiceList(addExtraService(store, action.payload)));
-  if (action.type == 'DELETE_EXTRA_SERVICE')
-    return dispatch(setExtraServiceList(deleteExtraService(store, action.payload)));
+  const params = { store, action, dispatch };
+
+  if (action.type == 'ADD_EXTRA_SERVICE') return addExtraService(params);
+  if (action.type == 'DELETE_EXTRA_SERVICE') return deleteExtraService(params);
 };
+
 export { registerGuestHouseInfoMiddleware };
 
-const addExtraService = (store, payload) => {
+const addExtraService = ({ store, action, dispatch }) => {
+  dispatch(setExtraServiceList(addServiceToArray(store, action.payload)));
+};
+
+const addServiceToArray = (store, payload) => {
   const { extraServiceList } = store.getState().registerGuestHouseInfoReducer;
   return extraServiceList.concat(payload);
 };
 
-const deleteExtraService = (store, payload) => {
+const deleteExtraService = ({ store, action, dispatch }) => {
+  dispatch(setExtraServiceList(deleteServiceFromArray(store, action.payload)));
+};
+
+const deleteServiceFromArray = (store, payload) => {
   const { extraServiceList } = store.getState().registerGuestHouseInfoReducer;
   const deleteIndex = extraServiceList.findIndex((service) => service == payload);
   extraServiceList.splice(deleteIndex, 1);
