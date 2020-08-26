7374b1f75eb45d60efcb456ce380c7345ede8dcd
Joy <hyewon3938@naver.com>
2020-08-27 06:00:34 +0900

    [#13] feat : 약관동의 데이터 가져오기 구현
    -  약관동의 가져오는 액션 함수 dispatch
    - useSelector로 apiReducer의 data 가져오기
    - data가 있는 경우에만 약관동의 컴포넌트 렌더링
    - termsAndCondition 컴포넌트의 props로 전달

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,24 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import { useSelector, useDispatch } from 'react-redux';
 
 import Header from '@/components/Header/Header';
 import CreateAccount from '@/components/Register/CreateAccount/CreateAccount';
 import TermsAndConditions from '@/components/Register/TermsAndConditions/TermsAndConditions';
 
+import { getTermsAndConditions } from '@/redux/actions/getTermsAndConditionsAction';
+
 const Register = () => {
+  const dispatch = useDispatch();
+  const { loading, data, error } = useSelector((state) => state.apiReducer);
+
   const [TermsAndConditionsDisplay, setTermsAndConditionsDisplay] = useState('flex');
   const [CreateAccountDisplay, setCreateAccountDisplay] = useState('none');
+  const [isAgreeToMarketing, setIsAgreeToMarketing] = useState(false);
+
+  useEffect(() => {
+    dispatch(getTermsAndConditions());
+  }, []);
 
   const nextButtonClickHandler = () => {
     setTermsAndConditionsDisplay('none');
     setCreateAccountDisplay('flex');
   };
 
+  const getIsAgreeToMarketing = (bool) => {
+    setIsAgreeToMarketing(bool);
+  };
+
   return (
     <Wrap>
       <Header />
-      <TermsAndConditions display={TermsAndConditionsDisplay} nextButtonClickHandler={nextButtonClickHandler} />
-      <CreateAccount display={CreateAccountDisplay} />
+      {data.length > 0 ? (
+        <TermsAndConditions
+          termsData={data}
+          display={TermsAndConditionsDisplay}
+          getIsAgreeToMarketing={getIsAgreeToMarketing}
+          nextButtonClickHandler={nextButtonClickHandler}
+        />
+      ) : (
+        ''
+      )}
+      <CreateAccount isAgreeToMarketing={isAgreeToMarketing} display={CreateAccountDisplay} />
     </Wrap>
   );
 };
