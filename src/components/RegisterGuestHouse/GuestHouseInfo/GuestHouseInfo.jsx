0f158c46d3079a2611e5652555a32b03040cf5f8
Joy <hyewon3938@naver.com>
2020-08-28 03:19:20 +0900

    feat : 게스트하우스정보 등록 api 응답 값에 따라 로직 처리
    - 게스트하우스정보 등록 api 요청 후 에러 시 에러 메시지 alert창으로 띄움
    - 성공적으로 post 했을 때 다음 버튼 로직 수행

--- src/components/RegisterGuestHouse/GuestHouseInfo/GuestHouseInfo.jsx
+++ src/components/RegisterGuestHouse/GuestHouseInfo/GuestHouseInfo.jsx
@@ -3,8 +3,11 @@ import styled from 'styled-components';
 import { setTimeout } from 'core-js';
 import { useSelector, useDispatch } from 'react-redux';
 
-import { addExtraService, deleteExtraService } from '@/redux/actions/extraServiceAction';
-import { postGuestHouseInfo } from '@/redux/actions/postGuestHouseInfoAction';
+import {
+  addExtraService,
+  deleteExtraService
+} from '@/redux/Registration/actions/RegisterGuestHouseActions/extraServiceAction';
+import { guestHouseInfoFormData } from '@/redux/Registration/actions/RegisterGuestHouseActions/guestHouseInfoFormDataAction';
 
 import { checkDuplicate } from '@/lib/util/checkDuplicate';
 
@@ -18,7 +21,7 @@ import UploadFile from '@/components/UIComponents/UploadFile/UploadFile';
 
 const GuestHouseInfo = ({ display, nextButton }) => {
   const dispatch = useDispatch();
-  const { extraServiceList } = useSelector((state) => state.registerGuestHouseInfoReducer);
+  const { extraServiceList, postGuestHouseInfo } = useSelector((state) => state.registerGuestHouseReducer);
 
   const [currentImage, setCurrentImage] = useState();
   const [serviceList, setServiceList] = useState([]);
@@ -42,6 +45,13 @@ const GuestHouseInfo = ({ display, nextButton }) => {
   useEffect(() => {
     setServiceList(extraServiceList);
   }, [extraServiceList]);
+
+  useEffect(() => {
+    const { data, loading, error } = postGuestHouseInfo;
+    if (!data) return;
+    if (data.status === 201) return nextButton();
+    if (error) alert(data.response.data.message);
+  }, [postGuestHouseInfo]);
 
   const validateName = () => {
     const nameValue = nameInput.current.value;
@@ -135,8 +145,8 @@ const GuestHouseInfo = ({ display, nextButton }) => {
         mainNumber: numberInput.current.value,
         image: currentImage
       };
-      dispatch(postGuestHouseInfo(postData));
-      nextButton();
+
+      dispatch(guestHouseInfoFormData(postData));
     }
   };
 
