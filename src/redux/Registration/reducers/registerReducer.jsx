94696776732fe5861d1183fb233e82aed86a0e92
Joy <hyewon3938@naver.com>
2020-08-29 01:43:28 +0900

    [#13] feat : 이메일, 닉네임 중복체크 데이터 registerReducer에 추가
    - 이메일, 닉네임 중복체크 데이터 registerReducer에 추가
    - 이메일, 닉네임 중복체크 타입 추가

--- src/redux/Registration/reducers/registerReducer.jsx
+++ src/redux/Registration/reducers/registerReducer.jsx
@@ -1,8 +1,22 @@
 import { reducerUtils, handleAsyncActions } from '@/lib/util/asyncUtils';
-import { GET_TERMS, GET_TERMS_SUCCESS, GET_TERMS_ERROR } from '@/redux/Registration/type/registerType';
+import * as registerType from '@/redux/Registration/type/registerType';
+
+const {
+  GET_TERMS,
+  GET_TERMS_SUCCESS,
+  GET_TERMS_ERROR,
+  CHECK_EMAIL,
+  CHECK_EMAIL_SUCCESS,
+  CHECK_EMAIL_ERROR,
+  CHECK_NAME,
+  CHECK_NAME_SUCCESS,
+  CHECK_NAME_ERROR
+} = registerType;
 
 const initialState = {
-  terms: reducerUtils.initial()
+  terms: reducerUtils.initial(),
+  checkEmail: reducerUtils.initial(),
+  checkName: reducerUtils.initial()
 };
 
 const registerReducer = (state = initialState, action) => {
@@ -10,8 +24,15 @@ const registerReducer = (state = initialState, action) => {
     case GET_TERMS:
     case GET_TERMS_SUCCESS:
     case GET_TERMS_ERROR:
-      const reducer = handleAsyncActions(GET_TERMS, 'terms');
-      return reducer(state, action);
+      return handleAsyncActions(GET_TERMS, 'terms')(state, action);
+    case CHECK_EMAIL:
+    case CHECK_EMAIL_SUCCESS:
+    case CHECK_EMAIL_ERROR:
+      return handleAsyncActions(CHECK_EMAIL, 'checkEmail')(state, action);
+    case CHECK_NAME:
+    case CHECK_NAME_SUCCESS:
+    case CHECK_NAME_ERROR:
+      return handleAsyncActions(CHECK_NAME, 'checkName')(state, action);
     default:
       return state;
   }
