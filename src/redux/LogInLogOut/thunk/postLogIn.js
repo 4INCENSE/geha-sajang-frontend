7f24c1eec6aeab60d8d6d780abae5d06f02477f6
Joy <hyewon3938@naver.com>
2020-08-30 22:13:03 +0900

    [#14] feat : 로그인 post thunk 함수 구현
    - 로그인 post thunk 함수 구현
    - 로그인 로그아웃 타입 파일 추가

---
+++ src/redux/LogInLogOut/thunk/postLogIn.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import { createPromiseThunk } from '@/common/lib/util/asyncUtils';
+import { POST_LOGIN } from '@/redux/LogInLogOut/type/LogInLogOutType';
+import { API_URL } from '@/common/config';
+
+const postLogInAPI = async (payload) => {
+  const postData = { account: payload.account, password: payload.password };
+  const response = await axios.post(API_URL.postLogIn, postData);
+  return response;
+};
+
+export const postLogIn = createPromiseThunk(POST_LOGIN, postLogInAPI);
