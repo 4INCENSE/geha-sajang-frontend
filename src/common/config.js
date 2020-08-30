ea010274b15b1575b90c4a63ea94b9d8c994826a
Joy <hyewon3938@naver.com>
2020-08-30 22:01:31 +0900

    feat : api URL 정보를 담은 config 파일 생성
    - api URL 정보를 담은 config 파일 생성

---
+++ src/common/config.js
@@ -0,0 +1,8 @@
+export const API_URL = {
+  postGuestHouseInfo: process.env.URL + process.env.POST_GUESTHOUSE_INFO,
+  getTerms: process.env.URL + process.env.GET_TERMS,
+  postCheckMail: process.env.URL + process.env.POST_CHECK_EMAIL,
+  postCheckName: process.env.URL + process.env.POST_CHECK_NAME,
+  postCreateAccount: process.env.URL + process.env.POST_CREATE_ACCOUNT,
+  postLogIn: process.env.URL + process.env.POST_LOGIN
+};
