f20bc7f251c017d095766d38fb17000ced30ab3b
Joy <hyewon3938@naver.com>
2020-09-01 07:49:23 +0900

    [#14] feat : 로그인 api 에러에 따라 모달창 띄우기 구현
    - 아무런 입력이 없을 경우 서버에 요청 전에 1.2초간 메시지 모달창 띄우기
    - 서버 요청 후 에러 시 메시지 모달창 띄우기
    - 이메일 인증되지 않은 계정의 경우 인증 메일 재전송 버튼 추가
    - 아직 이메일 재전송 api가 없어서 연결하지 않고 재전송되었다는 메시지 모달창만 띄우도록 구현

--- src/components/LogIn/LogInForm/AccountLogInForm/AccountLogInForm.jsx
+++ src/components/LogIn/LogInForm/AccountLogInForm/AccountLogInForm.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { useHistory } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
@@ -6,6 +6,9 @@ import { useSelector, useDispatch } from 'react-redux';
 import { postLogIn } from '@/redux/LogInLogOut/thunk/postLogIn';
 import { removeAccessToken } from '@/redux/LogInLogOut/actions/logInLogOutAction';
 
+import { logInErrorCode } from '@/common/constants/errorCode';
+
+import LoadingIndicator from '@/components/LoadingIndicator/LoadingIndicator';
 import Input from '@/components/UIComponents/Input/Input';
 
 const AccountLogInForm = ({ title, buttonTitle }) => {
@@ -14,19 +17,30 @@ const AccountLogInForm = ({ title, buttonTitle }) => {
 
   const { logIn } = useSelector((state) => state.logInLogOutReducer);
 
+  const [modalMessage, setModalMessage] = useState('계정 아이디 혹은 비밀번호가 일치하지 않습니다');
+  const [messageDisplay, setMessageDisplay] = useState('none');
+  const [buttonMessageDisplay, setButtonMessageDisplay] = useState('none');
+  const [isLoading, setIsLoading] = useState(false);
+
   const IdInput = React.createRef();
   const passwordInput = React.createRef();
 
   useEffect(() => {
-    const { data, loading, error } = logIn;
+    const { data, error } = logIn;
     if (!data) return;
+    if (data) setIsLoading(false);
     if (data.status === 200) return successLogIn();
-    if (error) alert(data.response.data.message);
+    if (error) errorLogIn(data);
   }, [logIn]);
 
   const logInButtonClickHandler = () => {
+    const idValue = IdInput.current.value;
+    const passwordValue = passwordInput.current.value;
+    if (idValue.length <= 0 || passwordValue <= 0) return setMessageModal('아이디와 비밀번호를 입력해주세요');
+
     const postData = { account: IdInput.current.value, password: passwordInput.current.value };
     dispatch(postLogIn(postData));
+    setIsLoading(true);
   };
 
   const passwordInputEnterKeyPressHandler = (e) => {
@@ -42,8 +56,53 @@ const AccountLogInForm = ({ title, buttonTitle }) => {
     console.log('로그아웃');
   };
 
+  const setMessageModal = (message) => {
+    setModalMessage(message);
+    setMessageDisplay('flex');
+    setTimeout(() => {
+      setMessageDisplay('none');
+    }, 1200);
+  };
+
+  const yesButtonClickHandler = () => {
+    setButtonMessageDisplay('none');
+    setMessageModal('인증 메일이 재전송되었습니다');
+  };
+
+  const noButtonClickHandler = () => {
+    setButtonMessageDisplay('none');
+  };
+
+  const errorLogIn = (data) => {
+    const errorCode = data.response.data.code;
+    const errorMessage = data.response.data.message;
+    const { NOT_CERTIFIED_ACCOUNT, NOT_FOUNT_ACCOUNT, INCORRECT_PASSWORD } = logInErrorCode;
+
+    if (errorCode === NOT_CERTIFIED_ACCOUNT) return setButtonMessageDisplay('flex');
+    if (errorCode === NOT_FOUNT_ACCOUNT || errorCode === INCORRECT_PASSWORD)
+      return setMessageModal('계정 혹은 비밀번호가 일치하지 않습니다');
+    setMessageModal(errorMessage);
+  };
+
   return (
     <>
+      {isLoading ? <LoadingIndicator position="absolute" /> : ''}
+      <LogInModal style={{ display: messageDisplay }}>
+        <Message>{modalMessage}</Message>
+      </LogInModal>
+      <LogInModal style={{ display: buttonMessageDisplay }}>
+        <ButtonMessage>
+          <span>
+            이메일 인증이 완료되지 않았습니다.
+            <br />
+            인증 메일을 재전송 하시겠습니까?
+          </span>
+          <ModalButtonWrap>
+            <ModalButton onClick={yesButtonClickHandler}>예</ModalButton>
+            <ModalButton onClick={noButtonClickHandler}>아니오</ModalButton>
+          </ModalButtonWrap>
+        </ButtonMessage>
+      </LogInModal>
       <LogInTitle>{title}</LogInTitle>
       <LogInFormWrap>
         <LogInFormContent style={{ marginTop: '15px' }}>아이디</LogInFormContent>
@@ -106,6 +165,7 @@ const InputWrap = styled.div`
 `;
 const ButtonWrap = styled.div`
   display: flex;
+  flex-direction: column;
   width: 350px;
   justify-content: center;
   margin-top: 20px;
@@ -119,9 +179,73 @@ const Button = styled.button`
   font-family: 'Eoe_Zno_L';
   color: white;
   font-size: 15px;
-  font-weight: 600;
+  font-weight: bold;
   margin: 0px 0px 18px 0px;
   &:hover {
     background: ${({ theme }) => theme.color.point};
+  }
+`;
+
+const LogInModal = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.8);
+`;
+
+const Message = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 80%;
+  min-height: 30%;
+  background: white;
+  border-radius: 5px;
+  padding: 20px 40px;
+  font-family: 'S-CoreDream-5Medium';
+  color: ${({ theme }) => theme.color.darkGray};
+  font-size: 14px;
+  line-height: 25px;
+`;
+
+const ButtonMessage = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 80%;
+  min-height: 35%;
+  background: white;
+  border-radius: 5px;
+  padding: 20px 40px;
+  span {
+    font-family: 'S-CoreDream-5Medium';
+    color: ${({ theme }) => theme.color.darkGray};
+    font-size: 14px;
+    line-height: 25px;
+  }
+`;
+
+const ModalButtonWrap = styled.div`
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+  margin-top: 25px;
+`;
+const ModalButton = styled.button`
+  width: 120px;
+  height: 35px;
+  background: ${({ theme }) => theme.color.point};
+  border-radius: 3px;
+  color: white;
+  font-family: 'Eoe_Zno_L';
+  color: white;
+  font-size: 15px;
+  font-weight: bold;
+  &:hover {
+    background: ${({ theme }) => theme.color.darkPoint};
   }
 `;
