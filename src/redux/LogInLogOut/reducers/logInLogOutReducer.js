6f2e18adfbd93ead7db3b30668ed44c2bf748e12
Joy <hyewon3938@naver.com>
2020-08-31 19:00:58 +0900

    [#14] feat : 로그인 성공 시 accessToken을 저장하는 리듀서 로직 추가
    - 로그인 성공 시 accessToken을 저장하는 리듀서 로직 추가

--- src/redux/LogInLogOut/reducers/logInLogOutReducer.js
+++ src/redux/LogInLogOut/reducers/logInLogOutReducer.js
@@ -1,5 +1,9 @@
 import { reducerUtils, handleAsyncActions } from '@/common/lib/util/asyncUtils';
-import { POST_LOGIN, POST_LOGIN_SUCCESS, POST_LOGIN_ERROR } from '@/redux/LogInLogOut/type/LogInLogOutType';
+import {
+  POST_LOGIN,
+  POST_LOGIN_SUCCESS,
+  POST_LOGIN_ERROR,
+} from '@/redux/LogInLogOut/type/logInLogOutType';
 
 const initialState = {
   accessToken: null,
@@ -9,9 +13,10 @@ const initialState = {
 const logInLogOutReducer = (state = initialState, action) => {
   switch (action.type) {
     case POST_LOGIN:
-    case POST_LOGIN_SUCCESS:
     case POST_LOGIN_ERROR:
       return handleAsyncActions(POST_LOGIN, 'logIn')(state, action);
+    case POST_LOGIN_SUCCESS:
+      return { ...state, logIn: reducerUtils.success(action.payload), accessToken: action.payload.data.accessToken };
     default:
       return state;
   }
