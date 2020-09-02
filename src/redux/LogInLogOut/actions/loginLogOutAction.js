48f0f9fe3326b3d53e5c9a5701a02b9a20c08bf1
Joy <hyewon3938@naver.com>
2020-09-02 21:58:43 +0900

    [#14] feat : 로그인 후 사용자 정보 localStorage에 저장
    - 로그인 성공 후 사용자의 accessToken과 registerState를 localStorage에 저장해서 사용

--- src/redux/LogInLogOut/actions/loginLogOutAction.js
+++
@@ -1,9 +0,0 @@
-import { REMOVE_ACCESS_TOKEN } from '@/redux/LogInLogOut/type/logInLogOutType';
-
-const removeAccessToken = () => {
-  return {
-    type: REMOVE_ACCESS_TOKEN
-  };
-};
-
-export { removeAccessToken };
