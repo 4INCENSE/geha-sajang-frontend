47c4994ff99f6de22f7ecc065cdefc03c3878f2d
Joy <hyewon3938@naver.com>
2020-09-03 02:45:18 +0900

    [#15] feat : restrictRoute fallback 파라미터를 함수로 받도록 변경
    - restrictRoute fallback을 리다리엑트가 아닌 함수로 받도록 변경

--- src/common/lib/util/RestrictRoute.jsx
+++ src/common/lib/util/RestrictRoute.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { Redirect } from 'react-router';
 
-export const RestrictRoute = ({ component: Component, fallback: Fallback, isAllow }) => {
-  return isAllow ? <Component /> : <Fallback />;
+const RestrictRoute = ({ component: Component, fallback, isAllow }) => {
+  return isAllow() ? <Component /> : fallback();
 };
 
 export default RestrictRoute;
