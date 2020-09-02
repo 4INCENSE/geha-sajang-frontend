9ac076c649dd7e3cec6546636ddc377fd813bf3a
Joy <hyewon3938@naver.com>
2020-09-02 23:02:16 +0900

    feat : 로그인 여부에 따라 제한적인 라우팅 로직 적용
    - 로그인하지 않고 로그인해야하는 페이지에 접근 시 로그인 페이지로 리다이렉트
    - 로그인 한 후에 로그인과 회원가입 페이지에 접근 시 메인 페이지로 리다이렉트

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,36 @@
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
+import { isAuthenticated } from '@/common/lib/authorityCheck';
 
 import GlobalStyleProvider from '@/components/style/GlobalStyle';
+import RestrictRoute from '@/common/lib/util/RestrictRoute';
+import Register from '@/components/Register/Register';
+import RegisterGuestHouse from '@/components/RegisterGuestHouse/RegisterGuestHouse';
 import LogIn from '@/components/LogIn/LogIn';
 import Main from '@/components/Main/Main';
-import Register from '@/components/Register/Register';
 import Footer from '@/components/Footer/Footer';
+import NotFound from '@/components/NotFound/NotFound';
+
+const goToMainPage = () => <Redirect to="/" />;
+const goToLogInPage = () => <Redirect to="/logIn" />;
 
 const App = () => {
   return (
     <GlobalStyleProvider>
       <BrowserRouter>
         <Switch>
-          <Route path="/" exact component={Main} />
-          <Route path="/LogIn" component={LogIn} />
-          <Route path="/SignUp" component={SignUp} />
-          <Route path="/register" component={Register} />
-          <Redirect path="*" to="/" />
+          <RestrictRoute path="/" component={Main} fallback={goToLogInPage} exact isAllow={isAuthenticated()} />
+          <RestrictRoute path="/logIn" component={LogIn} fallback={goToMainPage} isAllow={!isAuthenticated()} />
+          <RestrictRoute path="/register" component={Register} fallback={goToMainPage} isAllow={!isAuthenticated()} />
+          <RestrictRoute
+            path="/registerGuestHouse"
+            component={RegisterGuestHouse}
+            fallback={goToLogInPage}
+            isAllow={isAuthenticated()}
+          />
+          <Route path="/notFound" component={NotFound} />
+          <Redirect path="*" to="/notFound" />
         </Switch>
       </BrowserRouter>
       <Footer />
