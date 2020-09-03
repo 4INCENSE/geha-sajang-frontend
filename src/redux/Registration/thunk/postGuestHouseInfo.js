5b8cc8fdd3c4e3d4234340cc200d42d2b782de9b
Joy <hyewon3938@naver.com>
2020-09-03 15:55:20 +0900

    feat : 게스트하우스 등록 시 요청 헤더에 토큰 넣어 요청
    - 게스트하우스 등록 시 localStorage에서 토큰을 꺼내 요청 헤더에 토큰 담아 요청

--- src/redux/Registration/thunk/postGuestHouseInfo.js
+++ src/redux/Registration/thunk/postGuestHouseInfo.js
@@ -4,8 +4,9 @@ import { POST_GUEST_HOUSE_INFO } from '@/redux/Registration/type/registerType';
 import { API_URL } from '@/common/config';
 
 export const postGuestHouseInfoRequest = async (formData) => {
+  const accessToken = localStorage.getItem('jwt');
   const response = await axios.post(API_URL.postGuestHouseInfo, formData, {
-    headers: { 'Content-Type': 'multipart/form-data' }
+    headers: { 'Content-Type': 'multipart/form-data', Authorization: `Bearer ${accessToken}` }
   });
   return response;
 };
