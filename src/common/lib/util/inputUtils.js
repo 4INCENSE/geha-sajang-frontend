3f3db41412c5cd384d14a37ac758c1c30f931f64
Joy <hyewon3938@naver.com>
2020-09-06 02:31:22 +0900

    [#5] feat : 가격 입력 시 천 단위마다 콤마를 찍는 유틸 함수 추가
    - 자바스크립트의 toLocaleString 함수를 이용해서 구현
    - 전달된 numberValue에서 콤마가 있을 경우 콤마를 전부 제외한뒤 다시 콤마를 찍도록 구현

--- src/common/lib/util/inputUtils.js
+++ src/common/lib/util/inputUtils.js
@@ -2,3 +2,8 @@ export const removeChar = (value) => {
   const pattern = /[^0-9]/gi;
   return value.replace(pattern, '');
 };
+
+export const numberWithCommas = (numberValue) => {
+  const number = numberValue.replace(/,/gi, '');
+  return Number(number).toLocaleString();
+};
