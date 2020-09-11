2b60ae9ec36c7b16e59c914d92a65b5ca074b5cc
Joy <hyewon3938@naver.com>
2020-09-11 18:19:52 +0900

    [#9] feat : 헤더 UI 구현
    - 상단 헤더 UI 구현
    - 로그인한 경우 유저정보표시 UI구현

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,9 +2,36 @@ import React from 'react';
 import styled from 'styled-components';
 
 import logo from '@/img/logo/logo.png';
+  return (
+    <>
+      <CloseUserMenuWrap style={{ display: menuDisplay }} onClick={closeUserMenuWrapClickHandler}></CloseUserMenuWrap>
+      <Wrap>
+        <ServiceLogo src={logo} />
+        {accessToken ? (
+          <UserWarp onClick={userWrapClickHandler}>
+            <ProfileImage src={profileImg} />
+            <Nickname>
+              {nickname} <img src={downArrow} />
+            </Nickname>
+            <UserMenuWrap style={{ display: menuDisplay }}>
+              <UserMenu style={{ borderTop: 'none' }}>내 정보</UserMenu>
+              <UserMenu>설정</UserMenu>
+              <UserMenu onClick={logOutButtonClickHandler}>로그아웃</UserMenu>
+            </UserMenuWrap>
+          </UserWarp>
+        ) : (
+          ''
+        )}
+      </Wrap>
+    </>
+  );
+};
+
+export default Header;
 
 const Wrap = styled.div`
   display: flex;
+  justify-content: space-between;
   align-items: center;
   width: 100%;
   padding: 15px 25px;
@@ -15,12 +42,89 @@ const ServiceLogo = styled.img`
   height: 60px;
 `;
 
-const Header = () => {
-  return (
-    <Wrap>
-      <ServiceLogo src={logo} />
-    </Wrap>
-  );
-};
+const ButtonWrap = styled.div`
+  display: flex;
+`;
 
-export default Header;
+const Button = styled.button`
+  display: flex;
+  color: white;
+  font-weight: bold;
+  font-family: 'Eoe_Zno_L';
+  font-size: 16px;
+  &:hover {
+    color: ${({ theme }) => theme.color.gray};
+  }
+`;
+
+const UserWarp = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  min-width: 120px;
+  height: 50px;
+  background: white;
+  border-radius: 30px;
+  padding: 5px 10px;
+  z-index: 2;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const ProfileImage = styled.img`
+  width: 43px;
+  height: 43px;
+  border-radius: 30px;
+`;
+
+const Nickname = styled.div`
+  display: flex;
+  justify-content: center;
+  font-family: 'S-CoreDream-5Medium';
+  font-weight: bold;
+  font-size: 15px;
+  color: ${({ theme }) => theme.color.darkGray};
+  padding: 5px 5px 5px 10px;
+  margin-right: 3px;
+  img {
+    width: 13px;
+    margin-left: 10px;
+  }
+`;
+
+const UserMenuWrap = styled.div`
+  position: absolute;
+  top: 55px;
+  right: 0;
+  display: none;
+  width: 130px;
+  background: white;
+  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+  z-index: 2;
+`;
+
+const UserMenu = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 40px;
+  font-weight: bold;
+  font-family: 'Eoe_Zno_L';
+  color: ${({ theme }) => theme.color.darkGray};
+  border-top: solid 0.5px ${({ theme }) => theme.color.lightGray};
+  padding: 10px;
+  &:hover {
+    background: ${({ theme }) => theme.color.point};
+    color: white;
+  }
+`;
+
+const CloseUserMenuWrap = styled.div`
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+`;
