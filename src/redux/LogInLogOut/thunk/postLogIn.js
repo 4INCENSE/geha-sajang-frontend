1c0914885b156ed72e7439ab7830759612adf7bb
Joy <hyewon3938@naver.com>
2020-09-11 16:54:49 +0900

    [#14] feat : 로그인 후 응답값을 쿠키 값에 저장하는 기능 구현
    - registerState는 로컬스토리지에 그대로 저장 -> 쉽게 수정 가능하고 크리티컬한 정보가 아니기 때문에
    - 닉네임, 토큰, 프로필 이미지는 쿠키에 저장, 배포 후에는 백엔드에서 쿠키값을 생성 삭제하는데 개발 환경인 localhost에서는 쿠키가 저장되지 않기 때문에 프론트에서 쿠키 값을 저장

--- src/redux/LogInLogOut/thunk/postLogIn.js
+++ src/redux/LogInLogOut/thunk/postLogIn.js
@@ -2,11 +2,14 @@ import axios from 'axios';
 import { createPromiseThunk } from '@/common/lib/util/asyncUtils';
 import { POST_LOGIN } from '@/redux/LogInLogOut/type/logInLogOutType';
 import { API_URL } from '@/common/config';
+import { setCookie } from '@/common/lib/util/cookies';
 
 const postLogInRequest = async (payload) => {
   const postData = { account: payload.account, password: payload.password };
   const response = await axios.post(API_URL.postLogIn, postData);
-  localStorage.setItem('jwt', response.data.accessToken);
+  setCookie('jwt', response.data.accessToken);
+  setCookie('nickname', response.data.nickname);
+  setCookie('profileImage', response.data.profileImage);
   localStorage.setItem('registerState', response.data.registerState);
   return response;
 };
