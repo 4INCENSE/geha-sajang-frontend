9290b9b82dbd829ace663c81aad1d5b1588abb67
Joy <hyewon3938@naver.com>
2020-09-20 23:23:54 +0900

    [#17] feat : 예약등록 컴포넌트 기본 틀 UI 추가
    - 예약등록 컴포넌트의 기본 틀 UI 추가

--- src/components/Reservation/AddReservation/AddReservation.jsx
+++ src/components/Reservation/AddReservation/AddReservation.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
+
+import Header from '@/components/Header/Header';
 const AddReservation = () => {
-  return <Wrap></Wrap>;
+
+  return (
+    <>
+      <Header />
+      <Wrap>
+        <Title>예약 일정 등록</Title>
+        <ReservationWrap>
+        </ReservationWrap>
+      </Wrap>
+    </>
+  );
 };
 
 export default AddReservation;
@@ -13,5 +25,36 @@ const Wrap = styled.div`
   justify-content: center;
   align-items: center;
   width: 100%;
-  height: 730px;
+  padding: 60px 20px;
+  overflow-x: hidden;
+`;
+const Title = styled.div`
+  font-family: 'S-CoreDream-5Medium';
+  font-weight: bold;
+  font-size: 25px;
+  color: ${({ theme }) => theme.color.darkGray};
+  font-weight: bold;
+  margin: 0 0 40px 0;
+`;
+
+const ReservationWrap = styled.div`
+  background: white;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-around;
+  border: 0.5px solid #b3b3b3;
+  padding: 30px 20px;
+`;
+
+const ReservationContentWrap = styled.div`
+  width: 500px;
+  height: 700px;
+  padding: 20px;
+`;
+
+const DateWrap = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
 `;
