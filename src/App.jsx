2badc023fe1cfd103a502db7c2ae74dd36a497b1
Joy <hyewon3938@naver.com>
2020-09-20 23:28:30 +0900

    [#17] feat : react-dates 캘린더 스타일 커스텀 완료
    - 기존 스타일 위에 override 할 수 있는 css 파일 생성 및 적용
    - 글씨체 변경, 선택 날짜의 배경색, 글자색, 테두리 색 등을 변경

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ import AddReservation from '@/components/Reservation/AddReservation/AddReservati
 
 import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css';
+import '@/components/style/react_dates_overrides.css';
 
 const App = () => {
   const goToLogInPage = () => {
