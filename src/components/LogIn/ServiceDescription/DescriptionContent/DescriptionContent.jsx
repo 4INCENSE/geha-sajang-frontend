e16b9123f33b93767416c55dc4f2e6089be82be1
랄랄랄 <hyewon3938@naver.com>
2020-11-22 01:47:27 +0900

    [#19] feat : 데이터 index 값의 짝수 여부에 따라 다른 스타일 적용
    - index 값을 받아 짝수 여부를 확인하는 함수 구현
    - useEffect를 이용해 짝수 여부를 useState isEvenIndex에 저장
    - index 값이 짝수인 경우에 background는 회색, flex 속성은 reverse, border 위치는 오른쪽으로 변경

--- src/components/LogIn/ServiceDescription/DescriptionContent/DescriptionContent.jsx
+++ src/components/LogIn/ServiceDescription/DescriptionContent/DescriptionContent.jsx
@@ -4,15 +4,37 @@ import styled, { css, keyframes } from 'styled-components';
 import { loginSlideWidth, mobileModeWidth } from '@/common/constants/responsiveWidth';
 
 const DescriptionContent = ({ image, title, description, index }) => {
+  const [isEvenIndex, setIsEvenIndex] = useState();
+
+  const checkEven = (number) => {
+    return number % 2 ? false : true;
+  };
+
+  useEffect(() => {
+    setIsEvenIndex(checkEven(index));
+  }, []);
+
+  const backgroundStyle = !isEvenIndex ? { background: 'rgba(0,0,0,0.05)' } : {};
+
   return (
-    <Wrap>
-      <DescriptionWrap right>
-        <DescriptionImage right src={image} />
-        <DescriptionTitleWrap right>
-          <DescriptionTitle>{title}</DescriptionTitle>
-          <Description>{description}</Description>
-        </DescriptionTitleWrap>
-      </DescriptionWrap>
+    <Wrap style={backgroundStyle}>
+      {isEvenIndex ? (
+        <DescriptionWrap right>
+          <DescriptionImage right src={image} />
+          <DescriptionTitleWrap right>
+            <DescriptionTitle>{title}</DescriptionTitle>
+            <Description>{description}</Description>
+          </DescriptionTitleWrap>
+        </DescriptionWrap>
+      ) : (
+        <DescriptionWrap left>
+          <DescriptionTitleWrap left>
+            <DescriptionTitle>{title}</DescriptionTitle>
+            <Description>{description}</Description>
+          </DescriptionTitleWrap>
+          <DescriptionImage left src={image} />
+        </DescriptionWrap>
+      )}
     </Wrap>
   );
 };
@@ -42,6 +64,20 @@ const DescriptionWrap = styled.div`
   width: 100%;
   height: 100%;
   padding: 25px;
+  @media only screen and (max-width: ${loginSlideWidth}) {
+    ${(props) => {
+      if (props.right) {
+        return css`
+          flex-direction: column;
+        `;
+      }
+      if (props.left) {
+        return css`
+          flex-direction: column-reverse;
+        `;
+      }
+    }};
+  }
 `;
 
 const DescriptionImage = styled.img`
@@ -65,6 +101,23 @@ const DescriptionTitleWrap = styled.div`
   margin: 30px 0 0 0;
   padding: 30px 50px;
   word-break: keep-all;
+  ${(props) => {
+    if (props.right) {
+      return css`
+        align-items: flex-end;
+        border-right: solid 15px ${({ theme }) => theme.color.darkGray};
+        text-align: right;
+      `;
+    }
+    if (props.left) {
+      return css`
+        align-items: flex-start;
+        border-left: solid 15px ${({ theme }) => theme.color.darkGray};
+        text-align: left;
+      `;
+    }
+  }};
+
   @media only screen and (max-width: ${loginSlideWidth}) {
     width: 80%;
     align-items: center;
